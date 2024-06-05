cba1625c0e15b6dff43a757cc9e20e361a01fe00
t-a1024 <133000386+t-a1024@users.noreply.github.com>
2024-06-05 23:19:30 +0900

    build完了

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.GITHUB_PAGES ? '/reactPractice/' : './',
+  build: {
+    outDir: "docs"
+  }
 })
